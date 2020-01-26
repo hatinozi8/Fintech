0b088b2786fa76a713f88dffae0dea14dd09f738
hatinozi <1bjm2204@gmail.com>
2020-01-26 22:05:39 +0900

    supported linux

--- kabumemo_fetch1.py
+++ kabumemo_fetch1.py
@@ -13,11 +13,12 @@ if pf == 'Windows':
     driverpath = 'F:\\work\\chromedriver_win32\\chromedriver.exe'
     browserpath = 'F:\\work\\GoogleChromePortable\\App\\Chrome-bin\\chrome.exe'
 elif pf == 'Linux':
-    driverpath = '/opt/google/chrome/google-chrome'
-    browserpath = '/opt/google/chromedriver'
+    driverpath= '/opt/google/chromedriver'
+    browserpath  = '/opt/google/chrome/google-chrome'
 
 
-opt = webdriver.ChromeOptions()
+#opt = webdriver.ChromeOptions()
+opt = Options()
 
 opt.binary_location=browserpath
 opt.add_argument('--headless')
@@ -26,26 +27,37 @@ opt.add_argument('--disable-gpu')
 opt.add_argument('--window-size=1280,1024')
 
 print(opt.binary_location)
-driver = webdriver.Chrome(executable_path=driverpath,chrome_options=opt)
+#driver = webdriver.Chrome(executable_path=driverpath,chrome_options=opt)
+driver = webdriver.Chrome(driverpath,options=opt)
 
-driver.get('https://www.google.co.jp/')
-driver.save_screenshot('test.png')
+#driver.get('https://www.google.co.jp/')
+#driver.save_screenshot('test.png')
 
 code = 6502
 
-url = f"https://kabuoji3.com/stock/{code}/2019/"
 
-driver.get(url)
-print('[URL] {0}'.format(driver.current_url))
-print('[Title] {0}'.format(driver.title))
+def get_day_data(code,year):
+	url = f"https://kabuoji3.com/stock/{code}/{year}/"
 
-html = driver.page_source
+	driver.get(url)
+	#print('[URL] {0}'.format(driver.current_url))
+	#print('[Title] {0}'.format(driver.title))
 
-#download_button = driver.find_element_by_class_name("btn_form btn_download")
-#download_button.click()
-#time.sleep(1)
+	html = driver.page_source
+	dfs = pd.read_html(html,encoding="utf-8")
+	df = dfs[0]
+	
+	return df
 
-print(html)
+years = [2000+i for i in range(20)]
 
-#download_button = driver.find_element_by_class_name("btn_form btn_download")
-#download_button.click()
+data = pd.DataFrame()
+for y in range(2000,2020):
+	print(y)
+	data_y = get_day_data(6502,y)
+	data = pd.concat([data,data_y])
+
+
+
+breakpoint()
+
